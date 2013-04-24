1cfd72696c1cb75656da017ed62332a20cd40511
manaten <manaten@manaten.net>
2013-04-24 08:37:56 -0700

    背景ナビをちゃんと動くようにした

--- pixel-world/index.php
+++ pixel-world/index.php
@@ -6,6 +6,9 @@
 	<link rel="icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico" />
 	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
 	<?php
 		if ( is_singular() && get_option( 'thread_comments' ) )
 			wp_enqueue_script( 'comment-reply' );
@@ -22,11 +25,11 @@
 			</div>
 			<div id="menu">
 				<ul>
-					<li><a class="top" title="トップページ" href="<?php bloginfo('url'); ?>"></a></li>
-					<li><a class="blog" title="ブログ" href="<?php bloginfo('url'); ?>/archives"></a></li>
-					<li><a class="gallery" title="ギャラリー" href="<?php bloginfo('url'); ?>/gallery"></a></li>
-					<li><a class="bbs" title="ドット絵掲示板" href="http://dot.manaten.net"></a></li>
-					<li><a class="twitter" title="Twitter" href="http://twitter.com/manaten"></a></li>
+					<li><a class="top" title="トップページ" href="<?php bloginfo('url'); ?>">トップページ</a></li>
+					<li><a class="blog" title="ブログ" href="<?php bloginfo('url'); ?>/archives">ブログ</a></li>
+					<li><a class="gallery" title="ギャラリー" href="<?php bloginfo('url'); ?>/gallery">ギャラリー</a></li>
+					<li><a class="bbs" title="ドット絵掲示板" href="http://dot.manaten.net">ドット絵掲示板</a></li>
+					<li><a class="twitter" title="Twitter" href="http://twitter.com/manaten">Twitter</a></li>
 				</ul>
 			</div>
 			<div id="search">
@@ -40,40 +43,35 @@
 		</div>
 	</header>
 
-	<nav class="bg-navigation">
-		<div class="nav-previous">
-			<?php is_single() ? previous_post_link('%link', '<span>&larr;</span>') : next_posts_link('<span>&larr;</span>');  ?>
-		</div>
-		<div class="nav-next">
-			<?php is_single() ? next_post_link('%link', '<span>&rarr;</span>') : previous_posts_link('<span>&rarr;</span>'); ?>
-		</div>
-	</nav>
-
-	<div id="main">
-		<div class="wrapper">
-			<div id="content">
-				<?php if ( is_404() || !have_posts() ) {
-					get_template_part( 'tpl/404' );
-				} else if ( is_page() ) {
-					get_template_part( 'tpl/page' );
-				} else if (is_search()) {
-					get_template_part( 'tpl/archive' );
-				} else { ?>
-					<div id="contentLeft">
-						<?php
-						if (is_archive() || is_category()) {
-							get_template_part( 'tpl/archive' );
-						} else {
-							get_template_part( 'tpl/loop' );
-						}
-						?>
-					</div>
-					<div id="contentRight">
-						<?php get_template_part( 'tpl/sidebar' ); ?>
-					</div>
-					<div id="contentFooter"></div>
-				<?php } ?>
-			</div>
+	<div class="bg-nav-previous">
+		<?php is_single() ? previous_post_link('%link', '<span class="space"></span><span class="icon">&larr;</span>') : next_posts_link('<span class="space"></span><span class="icon">&larr;</span>');  ?>
+	</div>
+	<div class="bg-nav-next">
+		<?php is_single() ? next_post_link('%link', '<span class="space"></span><span class="icon">&rarr;</span>') : previous_posts_link('<span class="space"></span><span class="icon">&rarr;</span>'); ?>
+	</div>
+	<div class="wrapper">
+		<div id="content">
+			<?php if ( is_404() || !have_posts() ) {
+				get_template_part( 'tpl/404' );
+			} else if ( is_page() ) {
+				get_template_part( 'tpl/page' );
+			} else if (is_search()) {
+				get_template_part( 'tpl/archive' );
+			} else { ?>
+				<div id="contentLeft">
+					<?php
+					if (is_archive() || is_category()) {
+						get_template_part( 'tpl/archive' );
+					} else {
+						get_template_part( 'tpl/loop' );
+					}
+					?>
+				</div>
+				<div id="contentRight">
+					<?php get_template_part( 'tpl/sidebar' ); ?>
+				</div>
+				<div id="contentFooter"></div>
+			<?php } ?>
 		</div>
 	</div>
 
