a54ca5857260fc367cfbce2e292d285c761c125d
manaten <manaten@manaten.net>
2013-05-03 23:18:13 -0700

    pageでタイトルを表示しないようにした。

--- pixel-world/tpl/page.php
+++ pixel-world/tpl/page.php
@@ -1,6 +1,5 @@
 <?php while ( have_posts() ) : the_post(); ?>
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<h1 class="entry-title"><?php the_title(); ?></h1>
 	<div class="entry-content">
 		<?php 
 		if(is_page("top")) {
