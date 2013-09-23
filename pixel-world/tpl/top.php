7b5fecc852f47e16f410a9513135eb29ed28919f
manaten <manaten@manaten.net>
2013-09-24 02:48:45 +0900

    fix; ウィジェットが利用不能になっていたので修正

--- pixel-world/tpl/top.php
+++ pixel-world/tpl/top.php
@@ -130,6 +130,6 @@
 </div>
 
 <div id="sidebar">
-	<a class="twitter-timeline"	href="https://twitter.com/manaten"	data-widget-id="290505626906394624">@manaten からのツイート</a>
-	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+	<a class="twitter-timeline"  href="https://twitter.com/manaten"  data-widget-id="382199794493423616">@manaten からのツイート</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </div>
